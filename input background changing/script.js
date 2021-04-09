e64395df357ebeaa09a3917eab75f48c2f409573
unknown <vikaspsmech18@gmail.com>
2021-04-09 18:41:27 +0530

    input-background-changing

---
+++ input background changing/script.js
@@ -0,0 +1,8 @@
+document.getElementById("submit").addEventListener("click", function(){
+var bgcolor = document.getElementById("bg").value;
+document.getElementById('bgdcolor').style.background = bgcolor;
+
+
+
+
+});
